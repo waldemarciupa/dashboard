18c75385574137a0c0229c840ab4e5716f6bcd73
waldemarciupa <waldemarciupa90@gmail.com>
2020-04-04 13:55:00 +0200

    Updates

---
+++ precache-manifest.3f2681f59344cd944796a02a0621c6a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4517c4abe4211f1a448f51d428b2eca",
+    "url": "//waldemarciupa.github.io/index.html"
+  },
+  {
+    "revision": "1544051b5dd7ab95550d",
+    "url": "//waldemarciupa.github.io/static/js/2.0d5ba5a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//waldemarciupa.github.io/static/js/2.0d5ba5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0595bb86138b967bf2",
+    "url": "//waldemarciupa.github.io/static/js/main.5656b9dc.chunk.js"
+  },
+  {
+    "revision": "c800944a4ce2cc3946f2",
+    "url": "//waldemarciupa.github.io/static/js/runtime-main.9930c473.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//waldemarciupa.github.io/dashboard/precache-manifest.727b2ecd579ae2a28cd29c891f6b1414.js"
+  "//waldemarciupa.github.io/precache-manifest.3f2681f59344cd944796a02a0621c6a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//waldemarciupa.github.io/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//waldemarciupa.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
